11fdc059925fe8c5d4f61a981a1649768e85453b
Dmitri <warstream55@gmail.com>
2021-12-28 00:36:02 +0300

    Всё ещё криво работает

--- newton_difference_method.py
+++ newton_difference_method.py
@@ -1,19 +1,29 @@
 from math import *
 
 
-def newton(func, a, b):
-    x = a
-    xi = eval(func)
-    xn =
-    while abs(xn - xi) < 0.001:
+def derivate(func, x, xn):
+    return (eval(func, {'x': xn}) - eval(func, {'x': x})) / (xn - x)
 
+
+def newton(func, a, b, ans):
+    x = b
+    xn = eval(func) / derivate(func, x, x - 0.01)
+    while abs(xn - x) > 0.001 and xn >= a:
+        xp = xn
+        xn = x - eval(func, {'x': x}) / derivate(func, x, xn)
+        x = xp
+    ans.append(xn)
+    if xn <= b:
+        newton(func, xn + 0.01, a, ans)
+    return ans
 
 
 def main():
     inp = [float(a) for a in input().split()]
     a, b = inp
     func = input()
-    print(newton(func, a, b))
+    ans = []
+    print(newton(func, a, b, ans))
 
 
 if __name__ == '__main__':
