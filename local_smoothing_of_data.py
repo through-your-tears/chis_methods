faa55fe502d698ad303a5618c35f4ffb3fd8ee42
Dmitri <warstream55@gmail.com>
2021-12-30 00:10:36 +0300

    Числаки 2 и 3

---
+++ local_smoothing_of_data.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from gauss import gauss
+from fractions import Fraction
+from copy import deepcopy
+
+
+def least_squares_method(n, m, k, points):
+    n = len(points)
+    b = [[Fraction(0) for j in range(m)] for i in range(m)]
+    for k in range(m):
+        for l in range(m):
+            for i in range(n):
+                b[k][l] += points[i][0] ** (k + l)
+        c = Fraction(0)
+        for i in range(n):
+            c += points[i][0] ** k + points[i][0]
+        b[k].append(c)
+    return gauss(b)
+
+
+def local_smoothing_of_data(n, m, k, points):
+    for i in range(n):
+        if n - i > k / 2:
+            y = least_squares_method(n, m, k, points[i: i + k // 2 + 1])
+            points[i][1] = y
+        else:
+            y = least_squares_method(n, m, k, points[i:])
+            points[i][1] = y
+    return points
+
+
+def main():
+    n, m, k = [int(a) for a in input().split()]
+    points = [[int(a) for a in input().split()] for i in range(n)]
+    plt.scatter(np.array([deepcopy(a[0]) for a in points]), np.array([deepcopy(a[1]) for a in points]))
+    plt.show()
+    result = local_smoothing_of_data(n, m, k, points)
+    plt.scatter(np.array([deepcopy(a[0]) for a in result]), np.array([deepcopy(a[1]) for a in result]))
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
--- newton_difference_method.py
+++ newton_difference_method.py
@@ -2,28 +2,42 @@ from math import *
 
 
 def derivate(func, x, xn):
-    return (eval(func, {'x': xn}) - eval(func, {'x': x})) / (xn - x)
+    b = eval(func)
+    x, xn = xn, x
+    a = eval(func)
+    return (a - b) / (x - xn)
 
 
-def newton(func, a, b, ans):
-    x = b
-    xn = eval(func) / derivate(func, x, x - 0.01)
-    while abs(xn - x) > 0.001 and xn >= a:
+def newton(func, a, b, eps=0.001):
+    x = (b + a) / 2
+    xn = x - eval(func) / derivate(func, x, x - 0.000000001)
+    while abs(xn - x) > eps and xn >= a:
         xp = xn
-        xn = x - eval(func, {'x': x}) / derivate(func, x, xn)
+        xn = xn - eval(func) / derivate(func, x, xn)
         x = xp
-    ans.append(xn)
-    if xn <= b:
-        newton(func, xn + 0.01, a, ans)
-    return ans
+    return xn
 
 
 def main():
     inp = [float(a) for a in input().split()]
-    a, b = inp
+    a, b, eps, eps1 = inp
     func = input()
     ans = []
-    print(newton(func, a, b, ans))
+    y = a
+    while y < b:
+        x = y
+        f1 = eval(func)
+        x += eps1
+        f2 = eval(func)
+        if abs(f1) < eps:
+            ans.append(x)
+        elif f1 * f2 < 0:
+            ans.append(newton(func, y, x, eps))
+        y += eps1
+    if len(ans) != 0:
+        print(*ans, sep='\n')
+    else:
+        print('На данном промежутке корни не найдены!!!')
 
 
 if __name__ == '__main__':
