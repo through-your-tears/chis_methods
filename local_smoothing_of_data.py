9231e1a93584d178d1729771965124385d0c8e21
Dmitri <warstream55@gmail.com>
2022-01-08 16:22:47 +0300

    Исправлен алгоритм

--- local_smoothing_of_data.py
+++ local_smoothing_of_data.py
@@ -6,53 +6,49 @@ from copy import deepcopy
 from math import *
 
 
-def least_squares_method(n, m, k, points):
+def least_squares_method(m, points):
     n = len(points)
-    b = [[Fraction(0) for j in range(m)] for i in range(m)]
+    b = [[Fraction() for j in range(m)] for i in range(m)]
     for k in range(m):
         for l in range(m):
             for i in range(n):
                 b[k][l] += points[i][0] ** (k + l)
-        c = Fraction(0)
+        c = Fraction()
         for i in range(n):
-            c += points[i][0] ** k + points[i][0]
+            c += points[i][0] ** k * points[i][1]
         b[k].append(c)
-    for row in b:
-        print(*row)
     return gauss(b)
 
 
 def local_smoothing_of_data(n, m, k, points):
     for i in range(n):
-        if n - i > k / 2:
-            op = points[i: i + k // 2 + 1]
-            y = least_squares_method(n, m, k, op)
-            s = 0
-            for j in range(len(y)):
-                s += op[j][0]**i * y[j]
-            points[i][1] = s
+        if k // 2 < i < n - k // 2:
+            op = points[i - k // 2: i + k // 2 + 1]
+        elif k // 2 > i:
+            op = points[:i + k // 2 + 1]
         else:
-            op = points[i:]
-            y = least_squares_method(n, m, k, op)
-            s = 0
-            for j in range(len(y)):
-                s += op[j][0] ** i * y[j]
-            points[i][1] = s
+            op = points[i - k // 2:]
+        y = least_squares_method(m, op)
+        s = 0
+        for j in range(len(y)):
+            s += points[i][0] ** j * y[j]
+        points[i][1] = s
     return points
 
 
 def main():
     m, k, a, b = [int(a) for a in input().split()]
-    n = b - a
+    n = b - a + 1
     points = []
     func = input()
-    for x in range(a, b + 1):
-        points.append([x, eval(func)])
+    i = a
+    while i < b + 1:
+        x = Fraction(i)
+        points.append([x, Fraction(eval(func))])
+        i += 0.1
     plt.plot(np.array([deepcopy(a[0]) for a in points]), np.array([deepcopy(a[1]) for a in points]))
     plt.show()
     result = local_smoothing_of_data(n, m, k, points)
-    for row in result:
-        print(*row)
     plt.plot(np.array([deepcopy(a[0]) for a in result]), np.array([deepcopy(a[1]) for a in result]))
     plt.show()
 
