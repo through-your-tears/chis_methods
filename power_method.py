f9e9622079842b56f44227e54e58f8e7252e96a4
Dmitri <warstream55@gmail.com>
2022-02-14 10:46:56 +0300

    Исправлен алгоритм

--- power_method.py
+++ power_method.py
@@ -1,32 +1,40 @@
+from random import randint
 from SNAE_Newton import mul_matrix_on_vector
 
 
+def vec_length(a: list) -> float:
+    return sum([y**2 for y in a])**0.5
+
+
 def normalize(a: list) -> list:
-    return [x / (sum([y**2 for y in a])**0.5) for x in a]
+    return [x / vec_length(a) for x in a]
 
 
 def main():
-    N = 3
+    N = 4
+    DELTA = 0.001
+    EPS = 0.000000001
     for i in range(N):
         with open(f'tests0/test{i}.txt', 'r') as file:
             matrix = [[int(x) for x in line.split()] for line in file]
             n = matrix[0][0]
             matrix.pop(0)
-        y = [float(1) for x in range(n)]
+        y = [randint(0, 10000) for j in range(n)]
         x = normalize(y)
-        delta = 0.001
         yk = mul_matrix_on_vector(matrix, x)
         xk = normalize(yk)
-        lmbd = [0.1 for i in range(n)]
+        lmbd = [1 for i in range(n)]
         lmbdk = [yk[i] / x[i] for i in range(n)]
-        eps = 0.000000001
-        while max([lmbdk[i] - lmbd[i] for i in range(n)]) > eps:
-            y, x = yk, xk
+        flags = [True if abs(x[i]) > DELTA else False for i in range(n)]
+        while max([(lmbdk[i] - lmbd[i])**2 for i in range(n)])**0.5 > EPS:
+            x = xk
             yk = mul_matrix_on_vector(matrix, x)
             xk = normalize(yk)
             lmbd = lmbdk
-            lmbdk = [yk[i] / x[i] if abs(x[i]) > delta else lmbd[i] for i in range(n)]
-        print(f'Ответ на тест{i}: {sum(lmbdk) / n}, {xk}')
+            flags = [True if abs(x[i]) > DELTA else False for i in range(n)]
+            lmbdk = [yk[i] / x[i] if abs(x[i]) > DELTA else lmbd[i] for i in range(n)]
+            ans = [lmbdk[i] for i in range(n) if flags[i]]
+        print(f'Ответ на тест{i}: {sum(ans) / len(ans)}, {xk}')
         with open(f'tests0/answer{i}.txt', 'r') as f:
             print(f'проверка из файла с ответами: {f.readline()}')
 
