0936d0273f9e9de3b1deda3b13fd44285dd9e3d5
Dmitri <warstream55@gmail.com>
2022-03-24 02:40:02 +0300

    commit

--- 5-steps_adams_method.py
+++ 5-steps_adams_method.py
@@ -1,1 +1,22 @@
-"""Очередной итерационный метод, метод Ньютона пригодится, кстати"""
+from math import sqrt
+
+
+def euler(func, a, b, n, x, y):
+    coords = {}
+    h = (b - a) / n
+    for i in range(n):
+        y += h * func(x, y)
+        x += h
+        coords[str(x)] = y
+    return coords
+
+
+def adams(func, points, n, a, b):
+    coords = {}
+    return coords
+
+
+def main():
+    functions = [
+        lambda x, y: (3 * y + 2 * x * y) / (x ** 2),
+    ]
